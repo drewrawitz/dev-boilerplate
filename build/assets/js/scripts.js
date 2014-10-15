e1d928a50644e17a35f9cc9af8e58366c2420187
Drew Rawitz <email@drewrawitz.com>
2014-10-15 11:49:44 -0400

    removing compiled files from repo

--- build/assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Project Name
- * http://www.drewrawitz.com
- * @author Drew Rawitz <email@drewrawitz.com>
- * @version 1.0.0
- * Copyright 2014. MIT licensed.
- */
-!function(n){"use strict";n(function(){})}(jQuery,window,document);
